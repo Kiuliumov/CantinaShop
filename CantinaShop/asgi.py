2d08be1551f78a741f2cabc152eb09b8d413a099
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-07-14 18:59:16 +0300

    Still can't figure out why websockets don't work

--- CantinaShop/asgi.py
+++ CantinaShop/asgi.py
@@ -2,7 +2,7 @@ import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
-import CantinaShop.routing
+import sockets.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CantinaShop.settings')
 
@@ -10,7 +10,7 @@ application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            CantinaShop.routing.websocket_urlpatterns
+            sockets.routing.websocket_urlpatterns
         )
     ),
 })
--- sockets/consumers.py
+++ sockets/consumers.py
@@ -10,22 +10,23 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
-    async def receive(self, text_data):
-        data = json.loads(text_data)
-        message = data['message']
-        user = self.scope['user']
-        username = user.username if user.is_authenticated else 'Anonymous'
-        avatar_url = data.get('avatar_url', '')
+    async def receive(self, text_data=None, bytes_data=None):
+        if text_data:
+            data = json.loads(text_data)
+            message = data['message']
+            user = self.scope['user']
+            username = user.username if user.is_authenticated else 'Anonymous'
+            avatar_url = data.get('avatar_url', '')
 
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'chat_message',
-                'message': message,
-                'username': username,
-                'avatar_url': avatar_url,
-            }
-        )
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'chat_message',
+                    'message': message,
+                    'username': username,
+                    'avatar_url': avatar_url,
+                }
+            )
 
     async def chat_message(self, event):
         await self.send(text_data=json.dumps({
