296cde0722c0f7b1896633fffb8f0c7d0814c31a
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-07-15 22:00:58 +0300

    Tiny tweaks and decided to go for an api view bc why not

--- chat/views.py
+++ chat/views.py
@@ -9,6 +9,7 @@ from django.views import View
 from django.views.generic import TemplateView
 from django.contrib.auth import get_user_model
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from accounts.models import Account, UserModel
