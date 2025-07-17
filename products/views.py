1b1c390f567b191fb313bfc385b30050d18c144b
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-07-17 12:15:58 +0300

    Product add for admin

--- products/views.py
+++ products/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from msilib.schema import ListView
+from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from django.contrib import messages
+from django.shortcuts import redirect
+from .forms import ProductForm
+from products.models import Product
 
-# Create your views here.
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'products/product_list.html'
+    context_object_name = 'product_list'
+
+class AdminRequiredMixin(UserPassesTestMixin):
+    def test_func(self):
+        return self.request.user.is_staff or self.request.user.is_superuser
+
+    def handle_no_permission(self):
+        messages.error(self.request, "You must be an admin to access this page.")
+        return redirect('admin:login')
+
+class AddProductView(LoginRequiredMixin, AdminRequiredMixin, CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'products/add_product.html'
+    success_url = reverse_lazy('add-product')
+
+    def form_valid(self, form):
+        messages.success(self.request, "Product added successfully.")
+        return super().form_valid(form)
