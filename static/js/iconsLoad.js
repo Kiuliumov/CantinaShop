967ef0d6d9ef7d987a3981203f789a38e5c1a9c6
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-07-18 13:39:50 +0300

    Some refactoring

---
+++ static/js/iconsLoad.js
@@ -0,0 +1,8 @@
+  lucide.createIcons();
+
+  const menuBtn = document.getElementById('menu-btn');
+  const mobileMenu = document.getElementById('mobile-menu');
+
+  menuBtn.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+  });
