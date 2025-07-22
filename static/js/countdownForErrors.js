f4345cb6bab2f1f51021659517307998f66d4587
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-07-22 10:54:12 +0300

    Bugfixes

--- static/js/countdownForErrors.js
+++ static/js/countdownForErrors.js
@@ -1,11 +1,10 @@
 const countdown = document.getElementById('countdown');
-    const redirectUrl = "{% url 'index' %}"
     let seconds = parseInt(countdown.innerText);
 
     const timer = setInterval(() => {
       if (seconds <= 1) {
         clearInterval(timer);
-        window.location.href = redirectUrl;
+        window.location.href = window.redirectUrl;
       } else {
         seconds -= 1;
         countdown.innerText = seconds;
