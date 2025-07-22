e5007bcb9c87ba03f4243ec0add32c0c65337f33
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-07-22 14:06:28 +0300

    Started working on a contact form

--- common/models.py
+++ common/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+from django.views.generic import FormView
+from django.urls import reverse_lazy
+from django.contrib import messages
+from django.shortcuts import redirect
+from django import forms
+
+class ContactForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    email = forms.EmailField()
+    message = forms.CharField(widget=forms.Textarea)
+
+class ContactView(FormView):
+    template_name = 'contacts.html'
+    form_class = ContactForm
+    success_url = reverse_lazy('contact')
+
+    def form_valid(self, form):
+        messages.success(self.request, "Thanks for reaching out! We'll get back to you soon.")
+        return super().form_valid(form)
