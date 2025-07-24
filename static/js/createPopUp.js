7ed553672503fe4c9708dfc55bffc50344f824bd
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-07-24 15:37:45 +0300

    Pop up stuff yay

--- static/js/createPopUp.js
+++ static/js/createPopUp.js
@@ -1,16 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const modal = document.getElementById('successModal');
-    const closeBtn = document.getElementById('successClose');
+  const modal = document.getElementById('successModal');
+  const closeBtn = document.getElementById('successClose');
 
-    if (localStorage.getItem('showSuccessModal') === 'true') {
-      modal.classList.remove('hidden');
-      localStorage.removeItem('showSuccessModal');
-    }
+  if (localStorage.getItem('showSuccessModal') === 'true') {
+    modal.classList.remove('hidden');
+    localStorage.removeItem('showSuccessModal');
 
-    if (closeBtn) {
-      closeBtn.addEventListener('click', () => {
-        modal.classList.add('hidden');
-        localStorage.removeItem('showSuccessModal');
-      });
-    }
-  });
+    setTimeout(() => {
+      modal.classList.add('hidden');
+    }, 6000);
+  }
+
+  if (closeBtn) {
+    closeBtn.addEventListener('click', () => {
+    localStorage.removeItem('showSuccessModal');
+    });
+  }
+});
