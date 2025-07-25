ef95328e4d855d3afad94a99ca4419ba8fd94108
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-07-25 17:54:30 +0300

    Email stuff

--- CantinaShop/settings.py
+++ CantinaShop/settings.py
@@ -97,9 +97,12 @@ ASGI_APPLICATION = 'CantinaShop.asgi.application'
 WSGI_APPLICATION = 'CantinaShop.wsgi.application'
 
 CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer"
-    }
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            'hosts': [os.getenv('REDIS_URL', 'redis://localhost:6379')],
+        },
+    },
 }
 
 AUTH_USER_MODEL = 'accounts.UserModel'
