f4e839bd23197b224df9945528d50828905a4d57
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-07-28 10:55:25 +0300

    Formatting

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,25 +1,33 @@
 from django.urls import path, include, reverse_lazy
-from .views import RegisterView, ActivateAccount, Login, Logout, AccountUpdateView, AccountDeactivateView, \
-    CustomPasswordResetView
 from django.views.generic import TemplateView
 from django.contrib.auth import views as auth_views
+
+from .views import (
+    RegisterView, ActivateAccount, Login, Logout,
+    AccountUpdateView, AccountDeactivateView,
+    CustomPasswordResetView
+)
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', Login.as_view(), name='login'),
     path('logout/', Logout.as_view(), name='logout'),
+
     path('', AccountUpdateView.as_view(), name='account'),
 
     path('activate/<uidb64>/<token>/', ActivateAccount.as_view(), name='activate'),
     path('email-confirmation-sent/', TemplateView.as_view(
         template_name='accounts/authentication/email_confirm.html'),
-         name='email_confirmation_sent'),
+        name='email_confirmation_sent'
+    ),
     path('activation-invalid/', TemplateView.as_view(
         template_name='accounts/authentication/invalid_activation.html'),
-         name='activation_invalid'),
+        name='activation_invalid'
+    ),
     path('activation-success/', TemplateView.as_view(
         template_name='accounts/authentication/activation_success.html'),
-         name='activation_success'),
+        name='activation_success'
+    ),
 
     path('reset/', include([
         path('', CustomPasswordResetView.as_view(), name='password_reset'),
