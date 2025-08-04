608b9882188637e0d5f6df02a5acc5d5734ee289
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-08-04 22:50:38 +0300

    Deployment ready

--- accounts/models.py
+++ accounts/models.py
@@ -1,24 +1,53 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
 from django.db import models
 from django.core.validators import MinLengthValidator
 from accounts.validators import NoProfanityValidator, PhoneNumberValidator
 
 
-class UserModel(AbstractUser):
-    is_active = models.BooleanField(default=False)
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Email must be provided')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True')
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class UserModel(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
     is_chat_banned = models.BooleanField(default=False)
-    email = models.EmailField(
-        unique=True,
-        error_messages={'unique': 'A user with that email already exists.'}
-    )
-    username = models.CharField(
-        max_length=150,
-        unique=True,
-        validators=[NoProfanityValidator()],
-    )
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
 
     def __str__(self):
-        return self.username
+        return self.email
+
+    def get_full_name(self):
+        return self.email
+
+    def get_short_name(self):
+        return self.email
 
 
 class Account(models.Model):
