b3467211552cb87c03aeb5f9f58645598e31dee5
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-08-04 22:54:23 +0300

    Deployment ready

--- accounts/models.py
+++ accounts/models.py
@@ -1,30 +1,49 @@
 from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.contrib.auth.models import AbstractUser, PermissionsMixin
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.core.validators import MinLengthValidator, EmailValidator
 from accounts.validators import NoProfanityValidator, PhoneNumberValidator
 
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
+    def create_user(self, email, username=None, password=None, **extra_fields):
         if not email:
             raise ValueError('Email must be provided')
+        if not username:
+            raise ValueError('Username must be provided')
+
+        validator = NoProfanityValidator()
+        try:
+            validator(username)
+        except ValidationError as e:
+            raise ValueError(f'Invalid username: {e.messages[0]}')
+
         email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=email, username=username, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password=None, **extra_fields):
+    def create_superuser(self, email, username=None, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
+
+        if not username:
+            raise ValueError('Username must be provided')
+
+        validator = NoProfanityValidator()
+        try:
+            validator(username)
+        except ValidationError as e:
+            raise ValueError(f'Invalid username: {e.messages[0]}')
 
         if extra_fields.get('is_staff') is not True:
             raise ValueError('Superuser must have is_staff=True')
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True')
 
-        return self.create_user(email, password, **extra_fields)
+        return self.create_user(email, username=username, password=password, **extra_fields)
 
 
 class UserModel(AbstractBaseUser, PermissionsMixin):
