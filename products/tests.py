f630629a48381d7279a3e97e164ba721acab46ce
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-08-05 09:05:52 +0300

    Deployment Ready

--- products/tests.py
+++ products/tests.py
@@ -1,7 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase, Client
 from django.urls import reverse
-from django.contrib.auth.models import User
 from products.models import Product, Category, Comment, Rating
 
 # ----------------------
